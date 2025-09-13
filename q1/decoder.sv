6625ae12f61f7e9be50f863176b21d3d45a2f0a2
Yaowen07 <yaowenf@kth.se>
2025-09-13 20:44:52 +0800

    Update decoder.sv

--- q1/decoder.sv
+++ q1/decoder.sv
@@ -3,6 +3,9 @@ module decoder (
   output logic [15:0] one_hot
 );
   // …
-  // Add your description here
+ always_comb begin
+   one_hot = 16'b0;
+   one_hot[binary]=1'b1;
+ end// Add your description here
   // …
 endmodule
